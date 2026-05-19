50d98c0d8b99c9c8ad40fa46db7c965f4ce37490
MJMINationalAcademy <kingbhai000002@gmail.com>
2026-05-20 02:03:12 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+/* M.J.M.I CONFIGURATION CENTER */
+const CONFIG = {
+    master_key: "7474", // ONLY works on Logo click
+    lock_time: 300000,   // 5 Minutes
+
+    passwords: {
+        syllabus_tab: "9898",
+        research_tab: "9797",
+        campus_tab:   "9797",
+        admin_tab:    "9797"
+    },
+
+    academic: {
+        admission: "9797", syllabus_shortcut: "9898", exams: "9797",
+        library: "9797", faculty: "9797", result: "9797",
+        timetable: "9797", honors: "9797", global: "9797",
+        alumni: "9797", ecert: "9797", skill: "9797"
+    },
+
+    syllabus_classes: {
+        nursery: "9898", lkg: "9898", ukg: "9898", class_i: "9898",
+        class_ii: "9898", class_iii: "9898", class_iv: "9898", class_v: "9898",
+        class_vi: "9898", class_vii: "9898", class_viii: "9898", class_ix: "9898"
+    },
+
+    research_buttons: {
+        lab: "9797", journal: "9797", grants: "9797", patent: "9797",
+        data: "9797", incubation: "9797", partners: "9797", archives: "9797",
+        compute: "9797", thesis: "9797", ethical: "9797", bio: "9797"
+    },
+
+    campus_buttons: {
+        map: "9797", eco: "9797", media: "9797", tour: "9797",
+        prayer: "9797", parking: "9797", store: "9797", safety: "9797",
+        bank: "9797", network: "9797", fire: "9797", facility: "9797"
+    },
+
+    admin_buttons: {
+        records: "9797", dean: "9797", acts: "9797", careers: "9797",
+        fees: "9797", epay: "9797", tenders: "9797", executive: "9797",
+        directory: "9797", grievance: "9797", sysadmin: "9797", privacy: "9797"
+    }
+};
